c9571c01013a097fff483d81c58d2c6b4b3d6d8d
Yana <yadivenko@gmail.com>
2023-02-02 21:00:10 +0300

    t1

---
+++ task 1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите M чисел через пробел: ");
+string? str = System.Console.ReadLine();
+var arr = str.Split(" ");
+int[] arrint = new int[arr.Length];
+int count=0;
+
+ for (int i = 0; i < arr.Length; i++)
+{
+    arrint[i]=Convert.ToInt32(arr[i]);
+    
+    if (arrint[i]>0) count++;
+}
+
+System.Console.WriteLine($"Введено {count} числа(чисел) больше 0");
+
